9d701a9c506924f636c1ce5e53b4f96b75c4e811
Victor Molina <Victormolinalvp@gmail.com>
2022-07-22 20:17:43 -0300

    Modificacion de las mecanicas de combate y cargado de los personajes

--- RPG/Program.cs
+++ RPG/Program.cs
@@ -3,117 +3,132 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 
 List<Personaje> ListadoDePj = new List<Personaje>();
+var rand = new Random();
 
-ListadoDePj = CargadoDePj();
-
-int Jugador1 = SeleccionarPj(1, 999);   //El numero 999 es porque es como un indefinido
-int Jugador2 = SeleccionarPj(2, Jugador1);
-int Combate = Lucha(ListadoDePj, Jugador1, Jugador2);
-
-if (Combate == 999){
-    Console.WriteLine("Empate");
-} else {
-    Console.WriteLine("Gano el jugador: "+Combate);
+//CARGADO DE PERSONAJES
+Console.WriteLine("Cargamos el listado de los personajes");
+for (int i = 1; i < 10; i++)
+{
+    Personaje Heroe = new Personaje();
+    Heroe = Heroe.cargarPersonaje(i);
+    ListadoDePj.Add(Heroe);
 }
+//FIN DEL CARGADO DE PERSONAJE
 
 
 
+//COMBATES DE TODOS LOS HEROES
+Console.WriteLine("Comenzamos con los combates");
+int MaximoDePersonajes = ListadoDePj.Count;
+for (int i = 0; i < MaximoDePersonajes-1; i++)
+{
+    Combate Pelea = new Combate();
 
+    Console.WriteLine($"Combate numero {i+1}");
 
-
-
-
-List<Personaje> CargadoDePj(){
-    for (int i = 0; i < 9; i++)
-    {
-        Personaje Heore = new Personaje();
-        Heore =     Heore.cargarPersonaje();
-        ListadoDePj.Add(Heore);
-    }
-    return ListadoDePj;
-}
-
-int SeleccionarPj(int numero, int PjSeleccionado){
-    int eleccion;
+    int Numero1 = rand.Next(0,ListadoDePj.Count);
+    int Numero2;
     do
     {
-        Console.WriteLine($"Jugador numero {numero} seleccione un personaje: ");
-        eleccion = Convert.ToInt16(Console.ReadLine());
-    } while (eleccion < 1 || eleccion > 9);
+        Numero2 = rand.Next(0,ListadoDePj.Count);
+    } while (Numero2 == Numero1);
 
-    while (eleccion == PjSeleccionado)
+    Personaje Heroe1 = ListadoDePj[Numero1];
+    Personaje Heroe2 = ListadoDePj[Numero2];
+
+    //COMBATE DE LOS DOS HEROES
+    for (int j = 1; j < 4; j++)
     {
-        Console.WriteLine("El personaje que selecciono ya esta seleccionado, por favor seleccione otro: ");
-        eleccion = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine($"Ataque numero {j}");
+        Heroe1.DatosDePj.Salud = Heroe1.DatosDePj.Salud - Pelea.PuntosDeCombate(Heroe2, Heroe1);
+        Heroe2.DatosDePj.Salud = Heroe2.DatosDePj.Salud - Pelea.PuntosDeCombate(Heroe1, Heroe2);
     }
-    return eleccion;
-}
+    //FIN DEL COMBATE DE LOS DOS HEROES
 
-int Lucha(List<Personaje> ListadoDePj, int Jugador1, int Jugador2){
-    Combate PeleaDePj = new Combate();
-    Random rand = new Random();
-    int EmpiezaAtacando = rand.Next(0,2);
-    switch (EmpiezaAtacando)
-    {
-        case 0:
-            Console.WriteLine("Empieza atacando el jugador 1");
-            //ListadoDePj[Jugador1].MostrarPersonaje();
-            break;
-        case 1:
-            Console.WriteLine("Empieza atacando el jugador 2");
-            //ListadoDePj[Jugador2].MostrarPersonaje();
-            break;
-    }
+    if (Heroe1.DatosDePj.Salud == Heroe2.DatosDePj.Salud){
+        Console.WriteLine("Los heroes tienen la misma salud");
+        int eleccionDelCombate;
 
-    for (int i = 0; i < 3; i++)
-    {
-        Console.WriteLine($"Ataque numero: {i}");
-        if (ListadoDePj[Jugador1].DatosDePj.Salud > 0 && ListadoDePj[Jugador2].DatosDePj.Salud > 0){
-            if (EmpiezaAtacando == 0){
-                ListadoDePj[Jugador2].DatosDePj.Salud = PeleaDePj.PuntosDeCombate(ListadoDePj[Jugador1], ListadoDePj[Jugador2]);
-                Console.WriteLine("Vida restante del Jugador2: "+ListadoDePj[Jugador2].DatosDePj.Salud);
-                ListadoDePj[Jugador1].DatosDePj.Salud = PeleaDePj.PuntosDeCombate(ListadoDePj[Jugador2], ListadoDePj[Jugador1]);
-                Console.WriteLine("Vida restante del Jugador1: "+ListadoDePj[Jugador1].DatosDePj.Salud);
-            } else {
-                ListadoDePj[Jugador1].DatosDePj.Salud = PeleaDePj.PuntosDeCombate(ListadoDePj[Jugador2], ListadoDePj[Jugador1]);
-                Console.WriteLine("Vida restante del Jugador1: "+ListadoDePj[Jugador1].DatosDePj.Salud);
-                ListadoDePj[Jugador2].DatosDePj.Salud = PeleaDePj.PuntosDeCombate(ListadoDePj[Jugador1], ListadoDePj[Jugador2]);
-                Console.WriteLine("Vida restante del Jugador2: "+ListadoDePj[Jugador2].DatosDePj.Salud);
+        do
+        {
+            Console.WriteLine("Desea que los heroes vuelvan a combatir? (0:SI, 1:NO): ");
+            eleccionDelCombate = Convert.ToInt16(Console.ReadLine());
+        } while (eleccionDelCombate != 0 || eleccionDelCombate != 1);
+
+        if (eleccionDelCombate == 0){
+            Console.WriteLine("Los Heroes volveran a combatir");
+            //NUEVO COMBATE DE LOS HEROES
+            for (int j = 1; j < 4; j++)
+            {
+                Console.WriteLine($"Ataque numero {j}");
+                Heroe1.DatosDePj.Salud = Heroe1.DatosDePj.Salud - Pelea.PuntosDeCombate(Heroe2, Heroe1);
+                Heroe2.DatosDePj.Salud = Heroe2.DatosDePj.Salud - Pelea.PuntosDeCombate(Heroe1, Heroe2);
             }
-        }
-    }
-
-    Console.WriteLine("Comprobando vida");
-
-    if (ListadoDePj[Jugador1].DatosDePj.Salud != 0 && ListadoDePj[Jugador2].DatosDePj.Salud != 0){
-        Console.WriteLine("Los heroes tienen mas de 0 de salud");
-        if (ListadoDePj[Jugador1].DatosDePj.Salud > ListadoDePj[Jugador2].DatosDePj.Salud){
-            Console.WriteLine("El Heroe 1 ha ganado el combate");
-            ListadoDePj[Jugador1].MejorarHabilidades(ListadoDePj[Jugador1]);
-            Console.WriteLine("Mejoramos sus habilidades");
-            return Jugador2;
+            //FIN DEL NUEVO COMBATE DE LOS DOS HEROES
         } else {
-            if (ListadoDePj[Jugador2].DatosDePj.Salud > ListadoDePj[Jugador1].DatosDePj.Salud){
-                Console.WriteLine("El Heroe 2 ha ganado el Combate");
-                ListadoDePj[Jugador2].MejorarHabilidades(ListadoDePj[Jugador2]);
-                Console.WriteLine("Mejoramos sus habilidades");
-                return Jugador1;
+            Console.WriteLine("Su eleccion fue de que los heroes no vuelvan a enfrentarse, pero lamentablemente uno de los heroes debe ser eliminado. Lo decidiremos al azar");
+            int numeroRandom = rand.Next(0,2);
+            if (numeroRandom == 0){
+                Console.WriteLine("El Heroe del jugador numero 1 sera eliminado");
+                ListadoDePj.Remove(Heroe1);
+            } else {
+                Console.WriteLine("El Heroe del jugador numero 2 sera eliminado");
+                ListadoDePj.Remove(Heroe2);
             }
         }
-        Console.WriteLine("Los heroes tiene la misma cantidad de vida");
-        return 999; //999 Es cuando la Salud de ambos heroes sean iguales.
+
+        if (Heroe1.DatosDePj.Salud > Heroe2.DatosDePj.Salud){
+            ListadoDePj.Remove(Heroe2);
+        } else {
+            ListadoDePj.Remove(Heroe1);
+        }
+    } else if(Heroe1.DatosDePj.Salud > Heroe2.DatosDePj.Salud){
+        ListadoDePj.Remove(Heroe2);
     } else {
-        if (ListadoDePj[Jugador1].DatosDePj.Salud == 0 && ListadoDePj[Jugador2].DatosDePj.Salud == 0){
-            Console.WriteLine("Ambos heroes tienen 0 puntos de salud");
-            return 999; //999 porque es indefinido lo que nos quiere decir que ambos tienen 0 de vida
-        } else {
-            if (ListadoDePj[Jugador1].DatosDePj.Salud == 0){
-                Console.WriteLine("El personaje 1 perdio el combate");
-                return Jugador1;
-            } else {
-                Console.WriteLine("El personaje 2 perdio el combate");
-                return Jugador2;
-            }
+        ListadoDePj.Remove(Heroe1);
+    }
+
+    Console.WriteLine($"Fin del combate numero {i+1}");
+}
+Console.WriteLine("Fin de todos los combates");
+//FIN DE LOS COMBATES
+Console.WriteLine("El Heroe ganador de los combates es: ");
+ListadoDePj[0].MostrarPersonaje();
+Console.WriteLine("CONGRATULATIONS Winner. It's the best of all time");
+
+/*
+//Archivo csv
+string ArchivoCSV = "Ganadores.csv";
+
+if (!File.Exists(ArchivoCSV)){
+    File.Create(ArchivoCSV);
+}
+
+
+
+
+
+
+
+
+
+
+
+void AgregarHeroesAlCSV(List<Personaje> ListadoDePj, int Jugador1, int Jugador2, int JugadorLoser){
+    
+    if (Jugador1 == JugadorLoser){
+        Console.WriteLine($"\nJugador 1 eliminado usando a: {ListadoDePj[Jugador1].DatosDePj.Nombre} - {ListadoDePj[Jugador1].DatosDePj.Tipo}");
+
+        using (StreamWriter Lectura = File.AppendText(ArchivoCSV)){
+            Lectura.WriteLine("Heroe Ganador"+"Nombre del Heroe"+"Apodo del Heroe"+"Tipo del Heroe"+"Salud");
+            Lectura.WriteLine(ListadoDePj[Jugador2].DatosDePj.Tipo + ";" + ListadoDePj[Jugador2].DatosDePj.Nombre + ";" + ListadoDePj[Jugador2].DatosDePj.Apodo + ";" + ListadoDePj[Jugador2].DatosDePj.Tipo + ";" + ListadoDePj[Jugador2].DatosDePj.Salud);
         }
+        string AdicionarLinea = $"{ListadoDePj[Jugador1].DatosDePj.Nombre}, {ListadoDePj[Jugador1].DatosDePj.Apodo} - {ListadoDePj[Jugador1].DatosDePj.Tipo} VS {ListadoDePj[Jugador2].DatosDePj.Nombre}, {ListadoDePj[Jugador2].DatosDePj.Apodo} - {ListadoDePj[Jugador2].DatosDePj.Tipo}  ==>  GANADOR: {ListadoDePj[Jugador2].DatosDePj.Nombre}, {ListadoDePj[Jugador2].DatosDePj.Apodo} - {ListadoDePj[Jugador2].DatosDePj.Tipo} con {ListadoDePj[Jugador2].DatosDePj.Salud} de salud restante\n";
+        File.AppendAllText(ArchivoCSV, AdicionarLinea);
+        ListadoDePj.Remove(ListadoDePj[Jugador1]);
+        
+
+
     }
 }
+*/
